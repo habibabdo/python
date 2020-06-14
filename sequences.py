d79d780e4846ba47a44bea882f0c893abefe5236
habibabdo <habibdesigner9971728@gmail.com>
2020-06-14 03:31:24 +0300

    sequences.py  added

---
+++ sequences.py
@@ -0,0 +1,15 @@
+name='Alice'
+coordinates=(10.0,20.0)
+names=['Alice','Habib','Abdo']
+
+
+print(name[0])
+print(coordinates[0])
+print(names[0])
+
+for i in range(len(name)):
+    print(name[i])
+for i in range(len(coordinates)):
+    print(coordinates[i])
+for i in range(len(names)):
+        print(names[i])
