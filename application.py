ee96d88644a2bfb98ac98e092497822762f74508
habibabdo <habibdesigner9971728@gmail.com>
2020-06-15 16:55:44 +0300

    some things changed

--- application.py
+++ application.py
@@ -5,3 +5,11 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World!"
+
+@app.route("/Habib")
+def Habib():
+    return "Hello, Habib!"
+
+@app.route("/Abdo")
+def Abdo():
+    return "Hello, Abdo!"
