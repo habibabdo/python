dca1b0668345a1fc675b4ad5b7f7ebf8a7d1319e
habibabdo <habibdesigner9971728@gmail.com>
2020-06-17 21:33:45 +0300

    Application1 changed

--- application1.py
+++ application1.py
@@ -1,12 +1,15 @@
-from flask import  Flask
+from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+
+#@app.route("/")
+#def index():
+#    return "Hello, World!"
+
 
 @app.route("/<string:name>")
 def hello(name):
     name = name.capitaliye()
-    return  f"<H1>Hello, {name}!</H1>"
+    return
+    "<H1>Hello, {name}!</H1>"
